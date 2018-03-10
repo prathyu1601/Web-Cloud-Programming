a1135a58d5fce7fb8a0ac8e1f605e1a6d7a9b149
prathyu1601 <prathyusha1614@gmail.com>
2018-03-09 20:18:54 -0600

    Add files via upload

---
+++ Assignment-3/Source/app.js
@@ -0,0 +1,32 @@
+﻿// 'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('youtubeapp', [])
+
+    .controller('searchController', function ($scope, $http, $sce) {
+
+        $scope.getYoutubeLinks = function () {
+            var searchKey = document.getElementById("KeywordSearch").value;
+            $scope.YoutubeLink = new Array();
+            if (searchKey != null && searchKey != "") {
+
+                var handler = $http.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=" 
+                    + searchKey + "&key=AIzaSyBg4hWRNAzxjBQbBbrba8pBav0WRTbk-WI");
+
+                handler.then(function successCallback(data) {
+                    for (var i = 0; i < 5; i++) {
+
+                        var filter = data.data.items[i].id.videoId;
+                        console.log(data.data.items[i].snippet.title);
+                        $scope.YoutubeLink[i] = {
+                            "title": data.data.items[i].snippet.title,
+                            "vid": $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + filter)
+                        };
+                    }
+
+
+                });
+
+            }
+        }
+    });
